1111fc57d201bc4dc2af65019c0a04a544f46ef6
PKiruri <pkiruri06@gmail.com>
2022-09-28 06:31:44 -0700

    1st

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+
+
+
+def search_replace(my_list, search, replace):
+
+        newl = []
+
+            for i in my_list:
+
+                        if i == search:
+
+                                        newl += [replace]
+
+                                                else:
+
+                                                                newl += [i]
+
+                                                                    return newl
