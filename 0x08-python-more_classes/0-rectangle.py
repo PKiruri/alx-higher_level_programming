cf4f3a2510e67f91af02792ce0531e36a4abd7d2
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-10-18 19:36:31 +0300

    0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 """Rectangle module.
-
 This module contains a class that defines a rectangle.
-
 """
 
 
-
-
-
 class Rectangle():
+    """Defines a rectangle."""
 
-        """Defines a rectangle."""
-
-
-
-            pass
+    pass
