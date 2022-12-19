5af4752dd97e4fa2cf66022d1003124397cfa7b4
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-12-19 17:40:28 +0300

    5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,1 +1,7 @@
-Th
+-- Creates a table unique_id table in the current database,
+-- do nothing if the unique_id table already exists.
+
+CREATE TABLE IF NOT EXISTS `unique_id` (
+	`id` INT NOT NULL DEFAULT 1 UNIQUE,
+	`name` VARCHAR(256)
+);
