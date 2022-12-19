c6520f7ae50404e59b65b53c46dc5b09ded2e09b
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-12-19 15:19:19 +0300

    101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,15 @@
-Th
+#!/usr/bin/python3
+"""add_attribute module.
+Contains function that checks.
+"""
+
+
+def add_attribute(obj, name, value):
+    """Adds a new attribute to an object if it’s possible."""
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
