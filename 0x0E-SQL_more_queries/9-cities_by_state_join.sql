5facb0780e87eb07e559b83931bf3129167c1f6d
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-12-19 17:42:05 +0300

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,7 @@
-Th
+-- Lists all the cities found in the database hbtn_0d_usa.
+
+    SELECT c.id, c.name, s.name
+      FROM cities c
+INNER JOIN states s
+        ON c.state_id = s.id
+  ORDER BY c.id ASC;
