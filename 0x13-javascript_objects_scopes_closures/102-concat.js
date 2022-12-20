aa9d0db702e9f7fb3013aa6c43e095172301a805
PKiruri <107044946+PKiruri@users.noreply.github.com>
2022-12-20 10:13:49 +0300

    102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,8 @@
-Th
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
+
+fs.writeFileSync(process.argv[4], fileA + fileB);
