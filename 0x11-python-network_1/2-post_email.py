e3c76c0934bdec5ffa2469653b0f26eafbe277a1
PKiruri <107044946+PKiruri@users.noreply.github.com>
2023-01-19 12:08:28 +0300

    00

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,12 @@
-i
+#!/usr/bin/python3
+"""Python script that takes in a URLand an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8)"""
+
+from urllib import request, parse
+from sys import argv
+
+if __name__ == "__main__":
+    data = parse.urlencode({"email": argv[2]}).encode('utf-8')
+    with request.urlopen(argv[1], data) as page:
+        print(page.read().decode('utf-8'))
